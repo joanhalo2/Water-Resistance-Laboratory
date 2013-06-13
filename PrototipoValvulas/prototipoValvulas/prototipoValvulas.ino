18463b56e2f44cbef3c9be4b29bc0884dca00ae2
Joan Lopez <jslopezr00@gmail.com>
2013-06-13 17:57:59 -0500

    Se elimina el delay

--- PrototipoValvulas/prototipoValvulas/prototipoValvulas.ino
+++ PrototipoValvulas/prototipoValvulas/prototipoValvulas.ino
@@ -1,7 +1,8 @@
-int const delayValue=500;
+int const delayValue=3000;
 int const cant_valvulas = 30;
 int const pin_inicial = 22;
 int salidas[cant_valvulas];
+long previousMillis = 0;
 
 void setup() {
   Serial.begin(9600);
@@ -13,27 +14,34 @@ void setup() {
 }
 
 void loop() {
-  
-  for(int i=0; i<cant_valvulas; i++){
+  unsigned long currentMillis = millis();
+
+  if(currentMillis - previousMillis > delayValue) {
+    previousMillis = currentMillis;
     
-    int valorSalida = random(0,100);
-    int result;
-    
-    if(valorSalida>50){
-      result = HIGH;
-    }else{
-      result = LOW;
-    }
+    for(int i=0; i<cant_valvulas; i++){
+
+      int valorSalida = random(0,100);
+      int result;
+
+      if(valorSalida>50){
+        result = HIGH;
+      }
+      else{
+        result = LOW;
+      }
       digitalWrite(salidas[i], result);
-//    
-     if(i==cant_valvulas-1){
-       Serial.println(result);
-//     Serial.println(salidas[i]);
-      }else{
-       Serial.print(result);
-//    Serial.print(salidas[i]);
+      //    
+      if(i==cant_valvulas-1){
+        Serial.println(result);
+        //     Serial.println(salidas[i]);
+      }
+      else{
+        Serial.print(result);
+        //    Serial.print(salidas[i]);
       }
 
-  }
-  delay(delayValue);
+    }
+  }  
 }
+
